cef3dbf0e54e5331c91bedb02943c857d37cb5b4
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-03-17 11:04:52 +0100

    update MongoDB dsn

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config.js'); 
-const Crypto = require('./models/Crypto');
+const config = require('./config.js');
+const Crypto = require('./src/models/Crypto.js');
 
 // On lances notre serveur express qui sert le port 3000
 const app = express();
 const port = 3000;
 
-// On se connecte à la base de données
+// On récupère les identifiants de connexion à la base de données et on vérifie l'état de connexion
 mongoose.connect(config.mongoDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('Connection Error:', err));
+
+// On vérifie que l'on écoutes bien le port:3000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
