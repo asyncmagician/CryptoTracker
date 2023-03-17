a190f3444baca6c6cc732e1c3403d41b73af7fa0
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-03-17 11:32:51 +0100

    Revert "Update index.js to use the MVC logic"
    This reverts commit 0351cfde24d5e7f689f9b6927f3410bee6b4a6c9.

--- src/routes/cryptoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cryptoController = require('../controllers/cryptoController.js');
-
-const router = express.Router();
-
-router.get('/update-cryptos', cryptoController.updateCryptos);
-
-module.exports = router;
