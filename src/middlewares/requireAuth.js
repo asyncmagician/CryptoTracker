61bc2a960b59e9b28559039c2388349edca8d649
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-03-18 12:32:20 +0100

    Secure paths to require authentication

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -1,9 +1,9 @@
 const requireAuth = (req, res, next) => {
-    if (!req.session.userId) {
-      res.redirect('/');
-    } else {
-      next();
-    }
-  };
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+};
 
 module.exports = requireAuth;
