6a14843d3b15ef9d3a58b69ab1c24f66e3e268b9
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-03-18 12:01:56 +0100

    Create the authentication router + update the authentication script

--- public/script/authenticationListener.js
+++ public/script/authenticationListener.js
@@ -1,10 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.querySelector('#login-form');
-    
-    if (loginForm) {
-      loginForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        window.location.href = '/home';
+  const loginForm = document.querySelector('#login-form');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const email = document.querySelector('#email').value;
+      const password = document.querySelector('#password').value;
+
+      fetch('/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not OK');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Vérifier si la réponse contient un token valide
+        if (data.token) {
+          // Stockez le token dans le local storage du navigateur du client
+          localStorage.setItem('token', data.token);
+      
+          // Redirigez l'utilisateur vers la page d'accueil ou la page de tableau de bord
+          window.location.href = '/home';
+        } else {
+          const errorDiv = document.querySelector('#error');
+          if (errorDiv) {
+            errorDiv.innerHTML = 'The login is invalid.';
+            errorDiv.classList.add('alert', 'alert-danger');
+          }            
+        }
+      })
+      .catch(error => {
+        const errorDiv = document.querySelector('#error');
+        if (errorDiv) {
+          errorDiv.innerHTML = 'The login is invalid.';
+          errorDiv.classList.add('alert', 'alert-danger');
+        }
+        console.error(error);      
       });
-    }
-});
+    });      
+  }})
