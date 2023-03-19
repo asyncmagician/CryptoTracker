d7732c3ab0368d92669b09d94f3f319dc8f34c90
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-03-19 11:42:52 +0100

    Add cryptos fixtures

---
+++ tests/fixtures/cryptosFixtures.js
@@ -0,0 +1,148 @@
+module.exports = [
+    {
+      id: "bitcoin",
+      symbol: "btc",
+      name: "Bitcoin"
+    },
+    {
+      id: "ethereum",
+      symbol: "eth",
+      name: "Ethereum"
+    },
+    {
+      id: "binancecoin",
+      symbol: "bnb",
+      name: "Binance Coin"
+    },
+    {
+      id: "cardano",
+      symbol: "ada",
+      name: "Cardano"
+    },
+    {
+      id: "dogecoin",
+      symbol: "doge",
+      name: "Dogecoin"
+    },
+    {
+      id: "xrp",
+      symbol: "xrp",
+      name: "XRP"
+    },
+    {
+      id: "solana",
+      symbol: "sol",
+      name: "Solana"
+    },
+    {
+      id: "polkadot",
+      symbol: "dot",
+      name: "Polkadot"
+    },
+    {
+      id: "chainlink",
+      symbol: "link",
+      name: "Chainlink"
+    },
+    {
+      id: "litecoin",
+      symbol: "ltc",
+      name: "Litecoin"
+    },
+    {
+      id: "bitcoin-cash",
+      symbol: "bch",
+      name: "Bitcoin Cash"
+    },
+    {
+      id: "vechain",
+      symbol: "vet",
+      name: "VeChain"
+    },
+    {
+      id: "binance-usd",
+      symbol: "busd",
+      name: "Binance USD"
+    },
+    {
+      id: "terra-luna",
+      symbol: "luna",
+      name: "Terra"
+    },
+    {
+      id: "theta-token",
+      symbol: "theta",
+      name: "Theta Network"
+    },
+    {
+      id: "stellar",
+      symbol: "xlm",
+      name: "Stellar"
+    },
+    {
+      id: "avalanche",
+      symbol: "avax",
+      name: "Avalanche"
+    },
+    {
+      id: "ethereum-classic",
+      symbol: "etc",
+      name: "Ethereum Classic"
+    },
+    {
+      id: "algorand",
+      symbol: "algo",
+      name: "Algorand"
+    },
+    {
+      id: "matic-network",
+      symbol: "matic",
+      name: "Polygon"
+    },
+    {
+      id: "pancakeswap",
+      symbol: "cake",
+      name: "PancakeSwap"
+    },
+    {
+      id: "ftx-token",
+      symbol: "ftt",
+      name: "FTX Token"
+    },
+    {
+      id: "terrausd",
+      symbol: "ust",
+      name: "TerraUSD"
+    },
+    {
+      id: "wrapped-bitcoin",
+      symbol: "wbtc",
+      name: "Wrapped Bitcoin"
+    },
+    {
+      id: "cosmos",
+      symbol: "atom",
+      name: "Cosmos"
+    },
+    {
+      id: "compound-governance-token",
+      symbol: "comp",
+      name: "Compound"
+    },
+    {
+      id: "compound-ether",
+      symbol: "ceth",
+      name: "Compound Ether"
+    },
+    {
+      id: "uniswap",
+      symbol: "uni",
+      name: "Uniswap"
+    },
+    {
+      id: "okb",
+      symbol: "okb",
+      name: "OKB"
+    }
+];
+  
